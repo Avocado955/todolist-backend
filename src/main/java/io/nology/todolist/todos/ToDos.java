7b9e50b79a00bcc50948452c1ee308d8cbb86998
Avocado955 <mellis955@gmail.com>
2024-08-26 14:57:08 +1000

    adds the base entity class

--- src/main/java/io/nology/todolist/todos/ToDos.java
+++ src/main/java/io/nology/todolist/todos/ToDos.java
@@ -1,5 +1,57 @@
 package io.nology.todolist.todos;
 
-public class ToDos {
+import io.nology.todolist.common.BaseEntity;
+import jakarta.persistence.Column;
+
+public class ToDos extends BaseEntity {
+
+  // Constructor
+  public ToDos() {
+  }
+
+  // Properties
+  @Column
+  private String title;
+
+  @Column
+  private String description;
+
+  @Column // Change this eventually to a many to one (one category can have many ToDos)
+  private String category;
+
+  @Column
+  private Boolean isCompleted;
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getCategory() {
+    return category;
+  }
+
+  public Boolean getIsCompleted() {
+    return isCompleted;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public void setCategory(String category) {
+    this.category = category;
+  }
+
+  public void setIsCompleted(Boolean isCompleted) {
+    this.isCompleted = isCompleted;
+  }
 
 }
