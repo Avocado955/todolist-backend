f6cdd3fd51e15a036b3694d550e439c326fbd2c1
Avocado955 <mellis955@gmail.com>
2024-08-30 16:20:05 +1000

    adds the E2E test class and ther unit test class for Category, and adds a basic test for getAllCategories

---
+++ src/test/java/io/nology/todolist/category/CategoryEndToEndTest.java
@@ -0,0 +1,44 @@
+package io.nology.todolist.category;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class CategoryEndToEndTest {
+  @LocalServerPort
+  private int port;
+
+  @Autowired
+  private CategoryRepository categoryRepository;
+
+  @BeforeEach
+  public void setUp() {
+    RestAssured.port = port;
+    categoryRepository.deleteAll();
+    Category category1 = new Category();
+    category1.setName("code");
+    categoryRepository.save(category1);
+
+    Category category2 = new Category();
+    category2.setName("art");
+    categoryRepository.save(category2);
+  }
+
+  @Test
+  public void getAllCategories() {
+    given()
+        .when()
+        .get("/categories")
+        .then()
+        .statusCode(HttpStatus.OK.value());
+  }
+}
---
+++ src/test/java/io/nology/todolist/category/CategoryServiceUnitTest.java
@@ -0,0 +1,5 @@
+package io.nology.todolist.category;
+
+public class CategoryServiceUnitTest {
+
+}
