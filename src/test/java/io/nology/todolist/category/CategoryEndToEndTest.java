28d272ffd9de20fcd510cbdca631970626264a0a
Avocado955 <mellis955@gmail.com>
2024-08-30 17:11:19 +1000

    adds the working test for the e2e for Post-success, and adds the json schema validation package, adds broken e2e test for Post-failure

--- src/test/java/io/nology/todolist/category/CategoryEndToEndTest.java
+++ src/test/java/io/nology/todolist/category/CategoryEndToEndTest.java
@@ -9,7 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -39,6 +43,53 @@ public class CategoryEndToEndTest {
         .when()
         .get("/categories")
         .then()
-        .statusCode(HttpStatus.OK.value());
+        .statusCode(HttpStatus.OK.value())
+        .body("$", hasSize(2))
+        .body("name", hasItems("code", "art"))
+        // This schema is for when we get an array of objects back
+        .body(matchesJsonSchemaInClasspath("io/nology/todolist/category/schemas/categories-schema.json"));
+  }
+
+  @Test
+  public void createCategory_success() {
+    // Set up a DTO
+    CreateCategoryDTO data = new CreateCategoryDTO();
+    data.setName("new category");
+    given()
+        .contentType(ContentType.JSON)
+        .body(data)
+        .when()
+        .post("/categories")
+        .then()
+        .statusCode(HttpStatus.CREATED.value())
+        .body("name", equalTo("new category"))
+        .body("id", notNullValue())
+        // This schema is for when we get only one object back which is the Category
+        .body(matchesJsonSchemaInClasspath("io/nology/todolist/category/schemas/category-schema.json"));
+
+    // Check this category is in find all
+    given()
+        .when()
+        .get("/categories")
+        .then()
+        .statusCode(HttpStatus.OK.value())
+        .body("$", hasSize(3))
+        .body("name", hasItems("code", "art", "new category"));
+  }
+
+  @Test
+  public void createCategory_failure() {
+    // set up a DTO
+    CreateCategoryDTO data = new CreateCategoryDTO();
+    data.setName("code");
+    given()
+        .contentType(ContentType.JSON)
+        .body(data)
+        .when()
+        .post("/categories")
+        .then()
+        .statusCode(HttpStatus.BAD_REQUEST.value())
+        .body("errors.name[0]", equalTo("category with name 'code' already exists")); // This test current fails as
+                                                                                      // nothing is given back
   }
 }
