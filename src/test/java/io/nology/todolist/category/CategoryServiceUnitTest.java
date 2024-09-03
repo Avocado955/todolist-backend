5b201876284a300390277ee9135b9b2b5683c324
Avocado955 <mellis955@gmail.com>
2024-09-03 11:15:24 +1000

    adds github workflow yml to allow for continuous integration for testing whenever pushing or doing a pull request && adds a ToDoList unit test

--- src/test/java/io/nology/todolist/category/CategoryServiceUnitTest.java
+++ src/test/java/io/nology/todolist/category/CategoryServiceUnitTest.java
@@ -1,5 +1,38 @@
 package io.nology.todolist.category;
 
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
 public class CategoryServiceUnitTest {
+  @Mock
+  private CategoryRepository repo;
+
+  @InjectMocks
+  @Spy
+  private CategoryService categoryService;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void findAll() {
+    categoryService.findAll();
+    verify(repo).findAll();
+  }
+
+  @Test
+  public void findById() {
+    Long categoryId = 1L;
+    categoryService.findById(categoryId);
+    verify(repo).findById(categoryId);
+  }
 
 }
--- src/test/java/io/nology/todolist/todo/ToDoServiceUnitTest.java
+++ src/test/java/io/nology/todolist/todo/ToDoServiceUnitTest.java
@@ -1,5 +1,60 @@
 package io.nology.todolist.todo;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import io.nology.todolist.category.Category;
+import io.nology.todolist.category.CategoryService;
+import io.nology.todolist.common.exceptions.ServiceValidationException;
+import io.nology.todolist.todos.CreateToDosDTO;
+import io.nology.todolist.todos.ToDosRepository;
+import io.nology.todolist.todos.ToDosService;
+
 public class ToDoServiceUnitTest {
+
+  @InjectMocks
+  @Spy
+  private ToDosService service;
+
+  @Mock
+  private ToDosRepository repo;
+
+  @Mock
+  private CategoryService categoryService;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void createToDo_fails_whenCategoryIdInvalid() {
+    // given
+    CreateToDosDTO data = new CreateToDosDTO();
+    data.setCategoryId(1L);
+    data.setTask("This is the task");
+    data.setIsCompleted(false);
+    Optional<Category> result = Optional.empty();
+
+    // when
+    when(categoryService.findById(1L)).thenReturn(result);
+
+    // then
+    assertThrows(ServiceValidationException.class, () -> service.createToDo(data));
+
+    verify(repo, never()).save(any());
+  }
 
 }
